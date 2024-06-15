0e4192e8bd4ebf1c637c8a8afdfdef655a7ea112
YashAmin0 <yashamin024@gmail.com>
2024-06-15 08:51:50 +0530

    template added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'moiveRec',
+    version= '0.0.0',
+    author= 'Yash Amin',
+    author_email= 'yashamin024@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
